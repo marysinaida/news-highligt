539f3de4dbc2e43cc50764a2dfcd275b76cc30e2
marysinaida <marydorcassinaida54@gmail.com>
2019-11-16 11:33:22 +0300

    add a test instance method

--- app/news_test.py
+++ app/news_test.py
@@ -25,6 +25,18 @@ class TestArticles(unittest.TestCase):
     '''
     Test Class to test the behaviour of our Article class
     '''
+    def setUp(self):
+        '''
+         Set up method that will run before every Test
+         '''
+         self.new_article = Articles("the-verge","Daniel Kolitz","What's Going to Happen With Bitcoin?","Since its inception in 2009, Bitcoin has made and ruined fortunes, helped sell fentanyl and books about cryptocurrency, withstood literally millions of jokes and just as many predictions of imminent collapse, and—through a process opaque to most people, mysel…","https://gizmodo.com/whats-going-to-happen-with-bitcoin-1837940506", "https://i.kinja-img.com/gawker-media/image/upload/s--yDtXY-I4--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/pj5jc9ntilzdb4dfnivl.png","2019-10-07T12:00:00Z","This isn't surprising given Facebook's home turf and the usual strength of the US dollar. The absence of certain currencies might work in Libra's favor, too. The absence of China's yuan could appease American politicians scrutinizing the currency by assuaging… [+614 chars]")
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Articles))
+if if __name__ == "__main__":
+    unittest.main()
+
+
 
     
      
