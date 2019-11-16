3346b9d8139787b8552ecdca0739d77409cb7233
marysinaida <marydorcassinaida54@gmail.com>
2019-11-16 11:50:56 +0300

    add a class to define new Article object

--- app/models/news.py
+++ app/models/news.py
@@ -11,3 +11,18 @@ class News:
         self.category = category
         self.language = language
         self.country = country
+
+class Articles:
+    '''
+    Articles class to define Articles Objects
+    '''
+
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
