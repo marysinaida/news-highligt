1b9c91eedda544be34661d110997a357085df8be
marysinaida <marydorcassinaida54@gmail.com>
2019-11-16 11:08:54 +0300

    add method to run before ever test

--- app/news_test.py
+++ app/news_test.py
@@ -13,3 +13,13 @@ class TestNews(unittest.TestCase):
         self.new_news = News("abc-news","ABC News","Your trusted source for breaking news,analysis,exclusive interviews,headlines,and videos at ABCNews.com","https://abcnews.go.com","general","en","us")
 
     def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = News("abc-news","ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","https://abcnews.go.com","general","en","us")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+     
+
+
